2a7fec382b3433846c3a7dcd1b2792f028b0363e
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-09-14 17:35:49 +0530

    jenkins

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPwd { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+    }
+}
